49a8a600e01f2f48e4037a12f753569ee2794eca
Dan <danny_arthur@hotmail.co.uk>
2018-07-30 12:42:30 +0100

    Switch to ES6-style imports

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
-// https://github.com/umdjs/umd/blob/master/templates/nodeAdapter.js
-(function(define) {
-  define(function (require, exports, module) {
+import { processElement } from './processElement.js';
+import * as cheerio from 'cheerio';
 
-    var cheerio = require('cheerio');
-    var processElement = require('./processElement.js');
+const parseMedium = html => {
+  const $ = cheerio.load(html);
+  const title = $('h3').first().text();
+  const content = $('.section-inner').html();
+  // const markdown = processElement(content);
+  const markdown = $('.section-inner').contents().toArray().map(processElement).join('\n').replace(/\n\n\n/g, '\n\n');
+  return {
+    title,
+    markdown,
+  };
+};
 
-    const parseMedium = html => {
-      const $ = cheerio.load(html);
-      const title = $('h3').first().text();
-      const content = $('.section-inner').html();
-      // const markdown = processElement(content);
-      const markdown = $('.section-inner').contents().toArray().map(processElement).join('\n').replace(/\n\n\n/g,'\n\n');
-      return {
-        title,
-        markdown,
-      };
-    };
-
-    return parseMedium;
-
-  });
-
-}( // Help Node out by setting up define.
-  typeof module === 'object' && module.exports && typeof define !== 'function' ?
-    function (factory) { module.exports = factory(require, exports, module); } :
-      define
- ));
+export default parseMedium;
