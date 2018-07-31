154cee115cc50052634537592aaaee041566846b
Dan <danny_arthur@hotmail.co.uk>
2018-07-31 12:44:30 +0100

    Promise-based API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import { processElement } from './processElement.js';
 import * as cheerio from 'cheerio';
 
-const parseMedium = html => {
+const parseMedium = html => new Promise((resolve, reject) => {
   const $ = cheerio.load(html);
-  const title = $('h3').first().text();
+  const title = $('h1').first().text();
   const content = $('.section-inner').html();
   // const markdown = processElement(content);
-  const markdown = $('.section-inner').contents().toArray().map(processElement).join('\n').replace(/\n\n\n/g, '\n\n');
-  return {
-    title,
-    markdown,
-  };
-};
+  const elements = $('.section-inner').contents().toArray().map(processElement);
+  Promise.all(elements).then(results => {
+    const markdown = results.join('\n').replace(/\n\n\n/g, '\n\n');
+    resolve({
+      title,
+      markdown,
+    });
+  })
+});
 
 export default parseMedium;
--- src/processElement.js
+++ src/processElement.js
@@ -1,6 +1,6 @@
 import * as  cheerio from 'cheerio';
 
-export const processElement = element => {
+export const processElement = element => new Promise((resolve, reject) => {
   const $ = cheerio.load(element);
   const el = $(element).get(0);
 
@@ -8,75 +8,68 @@ export const processElement = element => {
     const text = $(el).text();
     const firstChar = text.substr(0, 1);
     if (['*', '-'].indexOf(firstChar) > -1) {
-      return `\\${text}`;
+      resolve(`\\${text}`);
+    } else {
+      resolve(text);
     }
-    return text;
-  }
-
-  if (el.type === 'tag') {
+  } else if (el.type === 'tag') {
 
     if (el.name === 'figure') {
       const caption = $(el).find('figcaption').text();
       // last() because the first img is low res
       const img = $('div > img').last();
       const src = img.attr('data-src') || img.attr('src');
-      return `\n![${caption}](${src})`;
-    }
+      resolve(`\n![${caption}](${src})`);
+    } else {
+      // Can't use .map() because it mutates the element
+      const p = [];
+      $(el).contents().each((i, e) => {
+        p.push(processElement(e));
+      });
 
-    // Can't use .map() because it mutates the element
-    const p = [];
-    $(el).contents().each((i, e) => {
-      p.push(processElement(e));
-    });
-    const processed = p.join('');
+      Promise.all(p).then(results => {
+        const processed = results.join('');
 
-    // const processed = children.each((i, el) => processElement(el)).join('');
-
-    if (el.name === 'em' || el.name === 'i') {
-      return `*${processed}*`;
+        if (el.name === 'em' || el.name === 'i') {
+          resolve(`*${processed}*`);
+        } else if (el.name === 'strong' || el.name === 'b') {
+          resolve(`**${processed}**`);
+        } else if (el.name === 'a') {
+          const href = $(el).attr('href');
+          resolve(`[${processed}](${href})`);
+        } else if (el.name === 'blockquote') {
+          resolve(`\n> ${processed}`);
+        } else if (el.name === 'h4') {
+          resolve(`\n### ${processed}`);
+        } else if (el.name === 'h3') {
+          resolve(`\n## ${processed}`);
+        } else if (el.name === 'h1') {
+          resolve(`\n# ${processed}`);
+        } else if (el.name === 'ul') {
+          resolve(`\n${processed}`);
+        } else if (el.name === 'li') {
+          resolve(`\n- ${processed}`);
+        } else if (el.name === 'p') {
+          resolve(`\n\n${processed}`);
+        } else if (el.name === 'img') {
+          const alt = $(el).attr('alt') || '';
+          const src = $(el).attr('src');
+          resolve(`![${alt}](${src})`);
+        } else if (el.name === 'div') {
+          resolve(`\n${processed}`);
+        } else if (['figure', 'div', 'figcaption'].indexOf(el.name) > -1) {
+          resolve(`\n${processed}`);
+        } else if (el.name === 'pre') {
+          resolve(`\n~~~\n${processed}\n~~~\n`);
+        } else {
+          console.log(`parse-medium: unprocessed tag <${el.name}>`);
+          resolve(`\n${processed}`);
+        }
+      }).catch(err => {
+        reject(err);
+      })
     }
-
-    if (el.name === 'strong' || el.name === 'b') {
-      return `**${processed}**`;
-    }
-
-    if (el.name === 'a') {
-      const href = $(el).attr('href');
-      return `[${processed}](${href})`;
-    }
-
-    if (el.name === 'blockquote') {
-      return `\n> ${processed}`;
-    }
-
-    // TODO Finish refactoring
-
-    if (el.name === 'h4') return `\n### ${processed}`;
-    if (el.name === 'h3') return `\n## ${processed}`;
-    if (el.name === 'h1') return `\n# ${processed}`;
-    if (el.name === 'ul') {
-      return `\n${processed}`;
-    }
-    if (el.name === 'li') {
-      return `\n- ${processed}`;
-    }
-    if (el.name === 'p') {
-      return `\n\n${processed}`;
-    }
-    if (el.name === 'img') {
-      const alt = $(el).attr('alt') || '';
-      const src = $(el).attr('src');
-      return `![${alt}](${src})`;
-    }
-    if (el.name === 'div') return `\n${processed}`;
-    if (['figure', 'div', 'figcaption'].indexOf(el.name) > -1) {
-      return `\n${processed}`;
-    }
-    if (el.name === 'pre') {
-      return `\n~~~\n${processed}\n~~~\n`;
-    }
-    console.log(`parse-medium: unprocessed tag <${el.name}>`);
-    return `\n${processed}`;
+  } else {
+    resolve(el);
   }
-  return el;
-};
+});
